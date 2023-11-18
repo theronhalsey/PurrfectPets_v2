5b2f33b13fccf131917fec9d804200e679d2fb65
Theron <theron.halsey@gmail.com>
2023-11-18 15:15:42 -0500

    revert

--- src/Utils/Petfinder.js
+++ src/Utils/Petfinder.js
@@ -1,14 +1,11 @@
-import dotenv from 'dotenv'
-dotenv.config();
-
-let accessToken = '';
+let accessToken;
 
 export const Petfinder = {
     async getAccessToken() {
         const requestUrl = `https://api.petfinder.com/v2/oauth2/token`;
         const response = await fetch(requestUrl, {
             method: 'POST',
-            body: `grant_type=client_credentials&client_id=${process.env.PETFINDER_API_KEY}&client_secret=${process.env.PETFINDER_API_SECRET}`,
+            body: `grant_type=client_credentials&client_id=${import.meta.env.VITE_API_KEY}&client_secret=${import.meta.env.VITE_API_SECRET}`,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
@@ -16,12 +13,10 @@ export const Petfinder = {
         const jsonResponse = await response.json();
         accessToken = jsonResponse.access_token;
         const expiresIn = jsonResponse.expires_in;
-        setTimeout(() => accessToken = '', expiresIn);
+        window.setTimeout(() => accessToken = '', expiresIn);
     },
     async getPets() {
-        if (accessToken === '') {
-            await Petfinder.getAccessToken()
-        }
+        await Petfinder.getAccessToken()
         const requestURL = 'https://api.petfinder.com/v2/animals?type=dog&page=2';
         return fetch(requestURL, {
             headers: {
@@ -35,7 +30,8 @@ export const Petfinder = {
             return response.json();
         })
         .then(data => {
-            return data.animals;
+            const pets = data.animals;
+            return pets;
         })
         .catch(error => {
             console.error('Error fetching pets: ', error);
